63ab30b6ce815fd8d586a6fe7af1f3ad0dcc8ee4
Eugene Ilin <jenyay.ilin@gmail.com>
2025-11-06 09:43:00 +0300

    Исправления в примерах про регулярные выражения

--- src/10. Regular expressions/example_10/find_variables.py
+++ src/10. Regular expressions/example_10/find_variables.py
@@ -4,7 +4,7 @@ text = """1. foo = 10 ;  bar  = 0.01; baz = 40
  236.   bar = 0.01;   baz= 30.1; foo =20.5
   42. foo= 0.5;  bar=10;  baz=900"""
 
-regex = re.compile(r"""(?:(?P<name>\w+)\s*=\s*(?P<val>\d+(?:\.\d+)?))""",
+regex = re.compile(r"""(?P<name>\w+)\s*=\s*(?P<val>\d+(?:\.\d+)?)""",
                    re.MULTILINE)
 
 for match in regex.finditer(text):
